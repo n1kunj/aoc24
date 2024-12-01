8ddbd203c1b9b992b5029c72c6c16c1bafbad11c
Nikunj Patel <np1509@gmail.com>
2024-12-01 12:10:24 +0000

    Day 1

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,6 +1,40 @@
+use std::{collections::HashMap, iter::zip};
+
 use crate::part_result::PartResult;
 
 pub fn main(input: &str, output: &mut PartResult) {
-    output.part1(0);
-    output.part2(0);
+    let mut lefts = Vec::<u64>::new();
+    let mut rights = Vec::<u64>::new();
+
+    for line in input.lines() {
+        let mut tokens = line.split_ascii_whitespace();
+        lefts.push(tokens.next().unwrap().parse::<u64>().unwrap());
+        rights.push(tokens.next().unwrap().parse::<u64>().unwrap());
+        assert!(tokens.next().is_none());
+    }
+    assert_eq!(lefts.len(), rights.len());
+
+    lefts.sort();
+    rights.sort();
+
+    let sum_diffs = zip(&lefts, &rights)
+        .map(|(l, r)| l.abs_diff(*r))
+        .sum::<u64>() as i64;
+
+    output.part1(sum_diffs);
+
+    let mut right_counts = HashMap::<u64, u64>::new();
+    for r in &rights {
+        right_counts.entry(*r).and_modify(|c| *c += 1).or_insert(1);
+    }
+
+    let sim_score = lefts
+        .iter()
+        .map(|l| match right_counts.get(l) {
+            Some(c) => l * c,
+            None => 0,
+        })
+        .sum::<u64>() as i64;
+
+    output.part2(sim_score);
 }
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,10 @@ use days::DAYS;
 
 use clap::Parser;
 use part_result::PartResult;
-use std::path::{Path, PathBuf};
+use std::{
+    path::{Path, PathBuf},
+    time::Instant,
+};
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -26,12 +29,11 @@ fn main() -> Result<(), String> {
     }
 
     let f = f.ok_or(format!("Unknown name {}", args.day))?;
-
-
+    let all_inputs_dir = format!("./inputs/{}", args.day);
 
     let load_and_run = |input: &str| {
         let path = |file_name: &str| -> PathBuf {
-            let path_name = format!("./inputs/{}/{}/{}.txt", args.day, input, file_name);
+            let path_name = format!("{}/{}/{}.txt", all_inputs_dir, input, file_name);
             Path::new(&path_name).to_owned()
         };
 
@@ -52,12 +54,32 @@ fn main() -> Result<(), String> {
     match &args.input {
         Some(input) => load_and_run(input),
         None => {
-            // TODO: Make input optional. When not specified:
-            // Enumerate all folders in the day's inputs
-            // Run the example first
-            // Run anything not called real next, alphabetically
-            // Run real last
-        },
+            let mut has_example = false;
+            let mut has_real = false;
+            let mut dir_names = Vec::<String>::new();
+            for dir in std::fs::read_dir(Path::new(&all_inputs_dir)).unwrap() {
+                let dir = dir.unwrap();
+                if dir.file_type().unwrap().is_dir() {
+                    match dir.file_name().to_str().unwrap() {
+                        "example" => has_example = true,
+                        "real" => has_real = true,
+                        other => dir_names.push(other.to_owned()),
+                    }
+                }
+            }
+            // Run example first
+            if has_example {
+                load_and_run("example");
+            }
+            // Then everything except real next
+            for dir_name in dir_names {
+                load_and_run(&dir_name);
+            }
+            // Then finally real
+            if has_real {
+                load_and_run("real");
+            }
+        }
     };
 
     Ok(())
@@ -73,7 +95,11 @@ fn run(
     println!("[{name}] Running...");
 
     let mut res = PartResult::new();
+    let before = Instant::now();
     f(&input, &mut res);
+    let after = Instant::now();
+    let dur = after - before;
+    println!("    [{name}] Took {dur:#?}");
 
     compare_result(name, 1, part1, res.get_part1());
     compare_result(name, 2, part2, res.get_part2());
